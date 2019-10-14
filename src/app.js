2afa6f27dac5812e0df1f83739dd26c78d655efa
james-roberts-web <jamesroberts@xponorth.co.uk>
2019-10-14 16:50:42 +0100

    priority list commit

--- src/app.js
+++ src/app.js
@@ -5,16 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
     el: "#app",
     data: {
       items: [
-        {task:"do the shopping", status:false},
-        {task:"wash the car", status:true},
-        {task:"mow the lawn", status:false},
-        {task:"take the bin out", status:false}
+        {task:"do the shopping", priority:"high" ,status:false},
+        {task:"wash the car", priority:"high", status:true},
+        {task:"mow the lawn",priority:"everyday", status:false},
+        {task:"take the bin out", priority:"high", status:false}
       ],
-      newItem:""
+      newItem:"",
+      prio:""
     },
     methods: {
       saveNewItem: function(){
-        this.items.push({task: this.newItem, status:false});
+        this.items.push({task: this.newItem, priority:this.prio, status:false});
         this.newItem = "";
       },
       doItem: function(index) {
